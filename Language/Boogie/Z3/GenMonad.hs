21b054b9580a0d49a73aa61f7171306b1d57034b
Scott West <scott.west@inf.ethz.ch>
2013-04-16 13:56:28 +0200

    Adding start of Custom type structure.

--- Language/Boogie/Z3/GenMonad.hs
+++ Language/Boogie/Z3/GenMonad.hs
@@ -3,7 +3,7 @@
 module Language.Boogie.Z3.GenMonad where
 
 import           Control.Applicative
-import           Control.Lens ((%=), uses, makeLenses)
+import           Control.Lens ((%=), view, _1, uses, makeLenses)
 import           Control.Monad.Trans.State
 import           Control.Monad.Trans
 
@@ -30,10 +30,15 @@ data Z3Env = Z3Env
                                               -- a tuple of them, and the
                                               -- associated constructor.
     , _sortMap :: Map Type Sort               -- ^ Maps types to sorts
-    , _customVals :: Map Int AST              -- ^ Map custom value tags to
-                                              -- their AST.
     , _refMap  :: Map TaggedRef AST           -- ^ Maps references to their
                                               -- Z3 AST node.
+    , _customVals :: Map Int AST              -- ^ Map custom value tags to
+                                              -- their AST.
+    , _customMap :: 
+        Map (Id,[Type])
+                (Sort, FuncDecl, FuncDecl)    -- ^ Map from identifier and
+                                              -- type arguments to a 
+                                              -- an uninterpreted type
     }
 
 makeLenses ''Z3Env
@@ -45,7 +50,7 @@ instance MonadZ3 Z3Gen where
 type Z3Gen = StateT Z3Env Z3
 
 emptyEnv :: Z3Env
-emptyEnv = Z3Env Map.empty Map.empty Map.empty Map.empty
+emptyEnv = Z3Env Map.empty Map.empty Map.empty Map.empty Map.empty
 
 evalZ3Gen :: Z3Gen a -> IO a
 evalZ3Gen act = evalZ3 $ evalStateT act emptyEnv
@@ -65,8 +70,6 @@ justElse = flip fromMaybe
 justElseM :: Monad m => Maybe a -> m a -> m a
 justElseM mb v = maybe v return mb
 
-
-
 lookupSort :: Type -> Z3Gen Sort
 lookupSort ttype =
     do sortMb <- uses sortMap (Map.lookup ttype)
@@ -85,8 +88,22 @@ lookupSort ttype =
                 do tupleArgSort <- lookupTupleSort argTypes
                    resSort <- lookupSort resType
                    mkArraySort tupleArgSort resSort
-            _ ->
-                error $ "typeToSort: cannot construct sort from " ++ show t
+            IdType ident types -> view _1 <$> lookupCustomType ident types
+
+lookupCustomType :: Id -> [Type] -> Z3Gen (Sort, FuncDecl, FuncDecl)
+lookupCustomType ident types =
+    do custMb <- uses customMap (Map.lookup (ident, types))
+       justElseM custMb $
+         do let str = customSymbol ident types
+            sym <- mkStringSymbol str
+            projSym <- mkStringSymbol (str ++ "_proj")
+            intSort <- lookupSort IntType
+            (sort, ctor, [proj]) <- mkTupleSort sym [(projSym, intSort)]
+            let res = (sort, ctor, proj)
+            customMap %= Map.insert (ident, types) res
+            return res
+                        
+            
 
 lookupTupleSort :: [Type] -> Z3Gen Sort
 lookupTupleSort types = ( \ (a,_,_) -> a) <$> lookupCtor types
@@ -108,6 +125,11 @@ lookupCtor types =
 -- | Type name for the symbol for the sort
 tupleSymbol :: [Type] -> String
 tupleSymbol ts = intercalate "_" (map typeString ts) ++ "SYMBOL"
+
+-- | Type name for the symbol for the sort
+customSymbol :: Id -> [Type] -> String
+customSymbol ident ts = intercalate "_" (ident : map typeString ts) ++ "_CUSTOM"
+
 
 -- | Symbol name for a type
 typeString :: Type -> String
