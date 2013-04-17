611495ca5271782996752c13d271a123edf9fbaf
Scott West <scott.west@inf.ethz.ch>
2013-04-17 17:57:44 +0200

    Using Expression creation helpers from Util.

--- Language/Boogie/Z3/Solver.hs
+++ Language/Boogie/Z3/Solver.hs
@@ -10,6 +10,7 @@ import           Language.Boogie.Z3.Solution
 import           Language.Boogie.Position
 import           Language.Boogie.Solver
 import           Language.Boogie.TypeChecker
+import           Language.Boogie.Util ((|=|), conjunction, enot)
 import           Language.Boogie.Z3.GenMonad
 
 import           System.IO.Unsafe
@@ -28,17 +29,10 @@ stepConstrs constrs = unsafePerformIO $ evalZ3Gen $
          Nothing -> return Nothing
 
 newConstraint :: Solution -> Expression
-newConstraint soln = not' (and' logicEqs)
+newConstraint soln = enot (conjunction logicEqs)
     where
-      p0 = Pos noPos
-      not' = p0 . UnaryExpression Not
-      lit = p0 . Literal
-      and' = foldr (\ x y -> p0 (BinaryExpression And x y)) 
-                   (lit (BoolValue True))
-      eq e1 e2 = p0 (BinaryExpression Eq e1 e2)
-
       logicEq :: Ref -> Expression -> Expression
-      logicEq r e = eq (p0 $ Logical (thunkType e) r) e
+      logicEq r e = gen (Logical (thunkType e) r) |=| e
       
       logicEqs :: [Expression]
       logicEqs = map (uncurry logicEq) (Map.toList soln)
