934fcf7f9a8fb767315a010be6c99b2f3bea118d
Nadia Polikarpova <nadia.polikarpova@gmail.com>
2013-09-18 19:09:05 +0200

    Bumped the version number.

--- Boogaloo.hs
+++ Boogaloo.hs
@@ -25,8 +25,8 @@ import Text.ParserCombinators.Parsec (parse, parseFromFile)
 import HtmlOutput
 
 programName = "boogaloo"
-versionName = "0.4"
-releaseDate = fromGregorian 2013 9 12
+versionName = "0.4.1"
+releaseDate = fromGregorian 2013 9 18
 
 -- | Execute or test a Boogie program, according to command-line arguments
 main = do
